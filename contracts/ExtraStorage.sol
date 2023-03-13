c8f2c0db4c260572968e3d74fce9b2c6f82e4065
MohammadRokib <rokibk548@gmail.com>
2023-03-13 11:32:34 +0600

    added ExtraStorage.sol

---
+++ contracts/ExtraStorage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// importing SimpleStorage contract into this contract.
+// all SimpleStorage functions can be used using variables in
+// ExtraStorage contract after importing.
+import "./SimpleStorage.sol";
+
+// inheriting SimpleStorage contract into this contract.
+// all SimpleStorage functions can be used directly without any variables
+// of ExtraStorage contract after inheriting.
+contract ExtraStorage is SimpleStorage {
+
+    // overriding the store function of SimpleStorage contract.
+    // in order to do that override keyword should be added
+    function store(uint256 _number) public override {
+        number = _number + 5;
+    }
+}
