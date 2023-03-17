9eaca7e9e1898f59c442518ff839e3d7c21023ff
MohammadRokib <rokibk548@gmail.com>
2023-03-17 07:34:45 +0600

    moved getPrice, getVersion, getConversionRate to PriceConverter.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,17 +6,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// importing the AggregatorV3Interface which we will use to get coin pricing
-// same as if I had pasted the code here
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     
     uint256 minUSD = 50 * 1e18;
         // minUSD is multiplied with 1e18 to match the decimal place
         // of the ETH value which is 1e18
     
-    // list of funders
+    // list of funders 
     address[] public funders;
 
     // apping the fund address to the fund amount
@@ -35,7 +34,21 @@ contract FundMe {
         // 1 ETH = 1e18 wei. values are calculated in wei in the code.
         // Thus we have to convert it if we want to use different unit
         // Error message in quote. If the condition is not met
-        require(getConversionRate(msg.value) >= minUSD, "Minimum funding amount is 1ETH");
+        // require(getConversionRate(msg.value) >= minUSD, "Minimum funding amount is 1ETH");
+            // The line above won't work since we've removed the getConversionRate function
+            // and put it in the PriceConverter library. Since we're using a library now
+            // similar task can be done by the line below:
+        require(msg.value.getConversionRate() >= minUSD, "Insufficient fund");
+            // getConversionRate(msg.value)
+            // msg.value.getConversionRate()
+            // these two are the same thing the only difference is first one is used
+            // for functions in a contract and the second one is used for function
+            // in library
+
+            // msg.value.getConversionRate()
+            // -- here msg.value is the first parameter for the getConversionRate
+            // function. If another parameter had to be passed then it would be
+            // written as: msg.value.getConversionRate(uint256 secondParameter)
 
         // Whenever a require statement is used if the condition inside it is
         // not met then the transaction will be cancelled and any prior work
@@ -46,47 +59,9 @@ contract FundMe {
 
         // mapping the sender address to sender's fund amount
         addToAmount[msg.sender] = msg.value;
-    }
 
-    // function to get the price of Ethereum or any coin we are working with
-    function getPrice() public view returns(uint256) {
-        // we will get the price of the coin using chainlink data feeds.
-        // there is a contract named AggregatorV3Interface which does this for us.
-        // there is a function inside it named getLatestPrice. and inside that
-        // function there is a variable named "int price" which has the value.
-
-        // this AggregatorV3Interface contract is outside of our project. so to
-        // interact with the contract we need two things of the contract:
-        // --1 ABI
-
-        // --2 Address
-            // [https://docs.chain.link/data-feeds/price-feeds/addresses/]
-            // 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e for Goerli testnet ETH/USD
-        
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (, int price,,,) = priceFeed.latestRoundData();
-        // the above line will return price of ETH in terms of USD
-        // the price will be something like this 3000_00000000
-
-        return uint256(price*1e10);
-        // the ETH value will be like this ------------> 1_000000000000000000
-        // the price we got is ---------------------> 3000_00000000
-        // multiplying price with 1e10 (10^10) -----> 3000_000000000000000000
-        // to make the decimal place of
-        // the price similar to ETH we're
-
-    }
-
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    // function to get the conversion rate of the coin we are using
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethUSD = (ethPrice * ethAmount) / 1e18;
-        return ethUSD;
+        // msg.value ---> number of wei sent with the message
+        // msg.sender --> address of the sender of the message
     }
 
     // function withdraw() {}
